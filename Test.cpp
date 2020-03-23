d00cbb1aeb927bea3f8524f99f7e76c6ae52b86a
verciucstefan <verciucs.sv@gmail.com>
2020-03-23 14:45:59 +0200

    Tema 4 final

--- Test.cpp
+++ Test.cpp
@@ -1,15 +1,35 @@
 #include "produs.h"
 #include "Repo.h"
 #include <assert.h>
+#include <iostream>
+
+using namespace std;
+
 void test()
 {
-    char nume[] = "biscuiti";
+    char nume[] = "biscuiti",nume1[]="papa";
     int data=5;
     int pret=23;
     Produs p(nume, data, pret);
+    Produs q(nume1, data, pret);
+    Produs k;
     Repo r;
     r.addProdus(p);
+    //r.addProdus(p);
+
     assert(r.size() == 1);
     assert(p.getPret() == 23);
+
+    Produs* a;
+    r.modificare(1, q);
+    a = r.getAll();
+    assert(a[0] == q);
+
+    r.delete_produs(1);
+    
+    assert(r.size() == 0);
+    cout << "Run test"<<'\n';
+    
+
 
 }
